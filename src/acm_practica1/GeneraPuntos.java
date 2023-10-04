95bde7b0e7a64c60125e5817e63558bf0ba76f0b
Manuel <manuel.silvan091@gmail.com>
2023-10-04 14:19:59 +0200

    Clase GeneraPuntos creada

---
+++ src/acm_practica1/GeneraPuntos.java
@@ -0,0 +1,14 @@
+package acm_practica1;
+
+public class GeneraPuntos {
+    Punto p1 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p2 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p3 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p4 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p5 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p6 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p7 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p8 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p9 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+    Punto p10 = new Punto((int)Math.floor(Math.random()*10+1), (int)Math.floor(Math.random()*10+1));
+}
