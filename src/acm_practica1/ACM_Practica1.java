a951394954c2779dc730118902e802ea46a30fe3
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-10-04 12:01:27 +0200

    commit inicial

---
+++ src/acm_practica1/ACM_Practica1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acm_practica1;
+
+/**
+ *
+ * @author Maria S
+ */
+public class ACM_Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
