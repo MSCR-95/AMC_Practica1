bfcf1dba494e467279442f3a4b6f8ba4efbebede
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-10-04 12:13:56 +0200

    añadir clase Punto

---
+++ src/acm_practica1/Punto.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acm_practica1;
+
+/**
+ *
+ * @author Maria S
+ */
+public class Punto {
+     private double x;
+    private double y;
+
+    public Punto() {
+        super();
+    }
+
+    public Punto(double x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Punto{" + "x=" + x + ", y=" + y + '}';
+    }
+}
