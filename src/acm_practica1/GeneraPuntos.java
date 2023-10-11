314eafda1d69e072148328267a9364aaa32acd9a
Manuel <manuel.silvan091@gmail.com>
2023-10-11 13:52:18 +0200

    ideas

--- src/acm_practica1/GeneraPuntos.java
+++ src/acm_practica1/GeneraPuntos.java
@@ -43,12 +43,67 @@ public class GeneraPuntos {
         return puntos;
     }
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
-     * public void LeePuntos() { try { List<String> allLines =
-     * Files.readAllLines(Paths.get("data\berlin52.tsp\berlin52.tsp"));
-     *
-     * for (String line : allLines) { System.out.println(line); } } catch
-     * (IOException e) { e.printStackTrace(); } }/*
+     * public void LeePuntos() { 
+     * try { 
+     * List<String> allLines = Files.readAllLines(Paths.get("src/data/berlin52.tsp/berlin52.tsp"));
+     * for (String line : allLines) { 
+     * System.out.println(line); 
+     * } 
+     * } 
+     * catch
+     * (IOException e) { 
+     * e.printStackTrace(); 
+     * } 
+     * }
+     */
+
+
+    /*  Path filePath = Paths.get("demo.txt");
+        Charset charset = StandardCharsets.UTF_8;
+     * try {
+            Files.lines(filePath, charset)
+                    .forEach(System.out::println);
+        } 
+
+        catch (IOException ex) {
+            System.out.format("I/O error: %s%n", ex);
+        }
+     * 
+     */
+    /*
+     * public class LineNumberReaderExample {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("demo.txt");
+        Charset charset = StandardCharsets.UTF_8;
+
+        try(BufferedReader bufferedReader = Files.newBufferedReader(filePath, charset);
+            LineNumberReader lineNumberReader = new LineNumberReader(bufferedReader)) {
+
+            String line;
+            while ((line = lineNumberReader.readLine()) != null) {
+                System.out.format("Line %d: %s%n", lineNumberReader.getLineNumber(), line);
+            }
+        } catch (IOException ex) {
+            System.out.format("I/O error: %s%n", ex);
+        }
+    }
+}
      */
 
 }
