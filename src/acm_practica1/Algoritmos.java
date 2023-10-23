a781d9643b36a176de8f8c73404e873b57bb906b
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-10-23 13:03:39 +0200

    Alg exhaustivo, refactorizacion, distancia

--- src/acm_practica1/Algoritmos.java
+++ src/acm_practica1/Algoritmos.java
@@ -4,6 +4,7 @@
  */
 package acm_practica1;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,34 +15,41 @@ import java.util.List;
 public class Algoritmos {
 
     public List<Punto> puntos = new ArrayList<>();
+     private DecimalFormat df = new DecimalFormat("#.########");
 
     public double busquedaExaustiva(List<Punto> punto) {
 
         double distancia = -1;
         double distanciaMin = Double.POSITIVE_INFINITY;
+        Punto x = null;
+        Punto y = null;
+        int nComparaciones = 0;
 
-        //ParDePuntos dosPuntos = new ParDePuntos();
         for (int i = 0; i < punto.size(); i++) {
             for (int j = 0; j < punto.size(); j++) {
                 //Controlamos que un punto no calcule la distancia con el mismo
                 if (punto.get(i) != punto.get(j)) {
-                    ParDePuntos dosPuntos = new ParDePuntos(punto.get(i), punto.get(j));
-
-                    distancia = dosPuntos.distancia();
                     
-                    //FILTRAMOS Y MOSTRAR¡MOS SOLO LA MENOR DISTANCIA DE UN PUNTO
+                    nComparaciones++;
+                    ParDePuntos dosPuntos = new ParDePuntos(punto.get(i), punto.get(j));
+                    distancia = dosPuntos.distancia();
+
+                    //FILTRAMOS
                     if (distancia < distanciaMin) {
 
                         distanciaMin = distancia;
-                        System.out.println("Punto " + i + ": " + punto.get(i) + "Punto " + j + ": " + punto.get(j) + "Distancia: " + distanciaMin + "\n");;
+                        x = punto.get(i);
+                        y = punto.get(j);
+                        //System.out.println("Punto " + i + ": " + punto.get(i) + "Punto " + j + ": " + punto.get(j) + "Distancia: " + distanciaMin + "\n");;
                     }
                     //AQUI NOS MUESTRA TODOS LOS PUNTOS Y TODAS SUS DISTANCIAS
                     //System.out.println("Punto " + i + ": " + punto.get(i) + "Punto " + j + ": " + punto.get(j) + "Distancia: " + distancia + "\n");
                 }
             }
         }
-        System.out.println("" );
-        System.out.println("Distancia Minima: " + distanciaMin);
+        //Mostramos los puntos y la distancia minima entre ellos
+        System.out.println("X: " + x + "Y: " + y + "Distancia: " + df.format(distanciaMin));
+        System.out.println("Numero de comparaciones: " + nComparaciones);
         return distanciaMin;
     }
 
