9410e79990a5454c67ce06de4d2919f73c42f472
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-11-16 09:54:43 +0100

    Parte1A: Todo pasado a Solucion Creada solucion2 para pruebas, la diferencia esq guarda los puntos y no los indices

--- src/acm_practica1/Solucion.java
+++ src/acm_practica1/Solucion.java
@@ -1,19 +1,20 @@
 package acm_practica1;
 
 public class Solucion {
-        public double dMin;
-        public int indiceP1; 
-        public int indiceP2;
-        public double time;
-        public int nComparaciones;
-        
-        public Solucion(){}
-        
-        public Solucion (double dMin, int indiceP1, int indiceP2, double time, int nComparaciones){
-            this.dMin = dMin;
-            this.indiceP1 = indiceP1;
-            this.indiceP2 = indiceP2;
-            this.time = time;
-            this.nComparaciones = nComparaciones;
-        }
+    public double dMin;
+    public int indiceP1;
+    public int indiceP2;
+    public double time;
+    public int nComparaciones;
+
+    public Solucion() {
+    }
+
+    public Solucion(double dMin, int indiceP1, int indiceP2, double time, int nComparaciones) {
+        this.dMin = dMin;
+        this.indiceP1 = indiceP1;
+        this.indiceP2 = indiceP2;
+        this.time = time;
+        this.nComparaciones = nComparaciones;
+    }
 }
